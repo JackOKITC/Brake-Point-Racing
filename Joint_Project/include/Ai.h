c3ecc392b8d170e0c90efe5c70905ab7fb020a34
(Student) - Denis John O Leary <c00201155@ITCARLOW.IE>
2017-03-13 09:58:49 +0000

    Made shell of ai class so that work can begin

---
+++ Joint_Project/include/Ai.h
@@ -0,0 +1,32 @@
+#ifndef AI_H
+#define AI_H
+
+#include <SFML\Graphics.hpp>
+
+class Ai
+{
+	Ai();
+	~Ai();
+
+	void update(double dt);
+	void render(sf::RenderWindow & window);
+
+	void moveCar();
+
+	sf::Vector2f m_position;
+
+private:
+
+	int m_nextNode;
+
+	sf::Texture m_carTex;
+	sf::Sprite m_carSprite;
+
+	sf::Vector2f m_velocity;
+	float m_rotation;
+	float m_speed = 0.0f;
+
+	const double DEG_TO_RAD = 3.14159 / 180.0f;
+};
+
+#endif // !AI_H
---
+++ Joint_Project/src/Ai.cpp
@@ -0,0 +1,21 @@
+#include "..\include\Ai.h"
+
+Ai::Ai()
+{
+}
+
+Ai::~Ai()
+{
+}
+
+void Ai::update(double dt)
+{
+}
+
+void Ai::render(sf::RenderWindow & window)
+{
+}
+
+void Ai::moveCar()
+{
+}
