a9f59dd3f5093cf69619fde53e1271999f5df835
(Student) - Denis John O Leary <C00201155@itcarlow.ie>
2017-03-14 20:58:36 +0000

    Added trigger acceleration for player

--- Joint_Project/src/Car.cpp
+++ Joint_Project/src/Car.cpp
@@ -14,6 +14,7 @@ Car::Car()
 	m_carSprite.scale(0.4, 0.4);
 	m_carSprite.setRotation(m_rotation);
 
+	m_carSprite.setOrigin(m_carSprite.getLocalBounds().width / 2, m_carSprite.getLocalBounds().height / 2);
 }
 
 Car::~Car() {}
@@ -21,14 +22,16 @@ Car::~Car() {}
 void Car::update(Xbox360Controller & controller, double dt)
 {
 	moveCar(controller);
+	
 	m_carSprite.setPosition(m_position);
 	m_carSprite.setRotation(m_rotation);
-	m_carSprite.setOrigin(m_carSprite.getLocalBounds().width / 2, m_carSprite.getLocalBounds().height / 2);
+	
 
 	sf::Vector2f newPos = sf::Vector2f(m_position.x + std::cos(DEG_TO_RAD  * (m_rotation - 90)) * m_speed * (dt / 1000),
 		m_position.y + std::sin(DEG_TO_RAD * (m_rotation - 90)) * m_speed * (dt / 1000));
 
 	m_position = newPos;
+	
 }
 
 void Car::render(sf::RenderWindow & window)
@@ -45,18 +48,36 @@ void Car::moveCar(Xbox360Controller & controller)
 	{
 		if (m_speed < 40)
 		{
-			m_speed += 0.01;
+			m_speed += -controller.m_currentState.triggers / 4000;
 		}
+	}
+	else if (!controller.m_currentState.RTrigger && m_speed > 0)
+	{
+		m_speed -= DECCELERATION_CAUSED_BY_FRICTION;
 	}
 
 	if (controller.m_currentState.LTrigger)
 	{
 		if (m_speed > -20)
 		{
-			m_speed -= 0.01;
+			m_speed -= controller.m_currentState.triggers / 5000;;
 		}
 	}
+	else if (!controller.m_currentState.LTrigger && m_speed < 0)
+	{
+		m_speed += DECCELERATION_CAUSED_BY_FRICTION;
+	}
 
+	std::cout << m_speed << std::endl;
+
+	//if (m_speed > 5 && (m_carSprite.getOrigin().y != m_carSprite.getLocalBounds().height - 10))
+	//{
+	//	m_carSprite.setOrigin(m_carSprite.getLocalBounds().width / 2, m_carSprite.getOrigin().y + 0.1);
+	//}
+	//else if (m_speed < 5 && (m_carSprite.getOrigin().y != 10))
+	//{
+	//	m_carSprite.setOrigin(m_carSprite.getLocalBounds().width / 2, m_carSprite.getOrigin().y - 0.1);
+	//}
 
 	// Checks if the down button has been pressed
 	if ((controller.m_currentState.dpadRight) || (controller.m_currentState.LeftThumbStick.x > 50))
@@ -72,4 +93,3 @@ void Car::moveCar(Xbox360Controller & controller)
 		m_rotation -= 0.1;
 	}
 }
-
