620d8d67b0e5aa25648773652132aa9c394705d3
(Student) - Jack O Kelly <C00203807@ITCARLOW.IE>
2017-03-23 17:20:47 +0000

    added sfemovie libraries and movie

---
+++ Joint_Project/include/Credits.h
@@ -0,0 +1,15 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+#include "Widget.h"
+#include "Xbox360Controller.h"
+#include <Sfe\Movie.hpp>
+
+class Credits
+{
+public:
+
+private:
+};
+
+#endif //!CREDITS_H
